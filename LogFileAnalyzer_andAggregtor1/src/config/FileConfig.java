ed06b6a7254ee6fcc5c18d398ff3abd2764ac694
achal2911 <achal.tikale@atyeti.com>
2025-05-22 18:12:18 +0530

    Delete LogFileAnalyzer_andAggregtor1 directory

--- LogFileAnalyzer_andAggregtor1/src/config/FileConfig.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public interface FileConfig {
-    String DIRECTORY_PATH = "resources_directory";
-    String ERROR_FILE_PATH = "resources_directory/errorFile.txt";
-    String DUPLICATE_FILE_PATH = "src/duplicate_resource_file/duplicateOf_app1.log";
-}
