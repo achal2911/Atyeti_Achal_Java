f745ea7a153559049c505c28745e29e08a1c04ba
achal2911 <achal.tikale@atyeti.com>
2025-07-29 17:13:35 +0530

    Implemented simple single-module Gradle Java project with JUnit 4 tests

---
+++ single-module-gradleProject/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperations operations = new
+                MathOperations();
+        operations.addition(3, 5);
+        operations.subtraction(5, 2);
+        operations.multiplication(3, 3);
+        operations.division(10, 5);
+    }
+}
